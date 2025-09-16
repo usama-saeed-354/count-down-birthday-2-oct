fc856bc8328e9cec23a176438e67b3b28871b1d6
usama-saeed-354 <33508803+usama-saeed-354@users.noreply.github.com>
2025-09-16 21:20:14 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Sep 30, 2025',
+  name: 'Fatima'
 };
 
 function hideEverything() {
