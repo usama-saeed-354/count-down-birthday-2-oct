5db7a5831051cd474b6c1dc8c26b4bf4c6953180
usama-saeed-354 <33508803+usama-saeed-354@users.noreply.github.com>
2025-10-07 10:04:52 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Oct 08, 2025',
+  birthdate: 'Oct 09, 2025',
   name: 'Bisma Sultan'
 };
 
